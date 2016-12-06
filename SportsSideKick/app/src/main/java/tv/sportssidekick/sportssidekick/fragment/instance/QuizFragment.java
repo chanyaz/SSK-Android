5d71940142abb573c2ddb39d764b180f03b0bb66
Filip Jovanovic <civonavoj.pilif@gmail.com>
2016-12-06 17:58:41 +0100

    Navigation and progress dialog for web views.

--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/instance/QuizFragment.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/instance/QuizFragment.java
@@ -1,13 +1,7 @@
 package tv.sportssidekick.sportssidekick.fragment.instance;
 
 
-import android.os.Bundle;
 import android.util.Base64;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import java.io.UnsupportedEncodingException;
 
@@ -22,28 +16,13 @@ import tv.sportssidekick.sportssidekick.fragment.BaseFragment;
  * A simple {@link BaseFragment} subclass.
  */
 
-public class QuizFragment extends BaseFragment {
-
+public class QuizFragment extends StoreFragment {
 
     public QuizFragment() {
         // Required empty public constructor
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
-        WebView webView = (WebView) view.findViewById(R.id.web_view);
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.setVisibility(View.VISIBLE);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // page loaded successfully!
-            }
-        });
+    protected void setupFragment(){
 
         // TODO Fill with proper data when User data is ready
         String userId = "123456789";
@@ -57,9 +36,9 @@ public class QuizFragment extends BaseFragment {
             String base64lastName = Base64.encodeToString(userId.getBytes("UTF-8"), Base64.DEFAULT);
             String base64nick = Base64.encodeToString(userId.getBytes("UTF-8"), Base64.DEFAULT);
 
-            String url = getResources().getString(R.string.quiz_url);
+            String urlTemp = getResources().getString(R.string.quiz_url);
 
-            StringBuilder urlBuilder = new StringBuilder(url);
+            StringBuilder urlBuilder = new StringBuilder(urlTemp);
             urlBuilder
                     .append("?userid=")
                     .append(base64userId)
@@ -70,13 +49,13 @@ public class QuizFragment extends BaseFragment {
                     .append("&nick=")
                     .append(base64nick);
 
-            webView.loadUrl(urlBuilder.toString());
-
+            url = urlBuilder.toString();
 
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
-        return view;
+
+        withNavigation = false;
     }
 
 }
--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/instance/StoreFragment.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/instance/StoreFragment.java
@@ -2,11 +2,15 @@ package tv.sportssidekick.sportssidekick.fragment.instance;
 
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import com.wang.avi.AVLoadingIndicatorView;
 
 import tv.sportssidekick.sportssidekick.R;
 import tv.sportssidekick.sportssidekick.fragment.BaseFragment;
@@ -21,31 +25,89 @@ import tv.sportssidekick.sportssidekick.fragment.BaseFragment;
 
 public class StoreFragment extends BaseFragment {
 
+    String url;
+    WebView webView;
+
+    Button backButton;
+    Button forwardButton;
+    Button refreshButton;
+    Button shareToWallButton;
+    View view;
+    AVLoadingIndicatorView progressBar;
+    View webContainer;
+    boolean withNavigation;
 
     public StoreFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_store, container, false);
-        WebView webView = (WebView) view.findViewById(R.id.web_view);
+        View view = inflater.inflate(R.layout.fragment_with_web_view, container, false);
 
+        webContainer = view.findViewById(R.id.navigation_web_container);
+
+        webView = (WebView) view.findViewById(R.id.web_view);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
-        webView.setVisibility(View.VISIBLE);
+
+        setupFragment();
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                // page loaded successfully!
+                webContainer.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+                Log.d("WEB VIEW", "Loaded successfully!");
             }
         });
-        String url = getResources().getString(R.string.store_url);
+
         webView.loadUrl(url);
+
+        progressBar = (AVLoadingIndicatorView) view.findViewById(R.id.login_login_progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        refreshButton = (Button) view.findViewById(R.id.refresh_button);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        forwardButton = (Button) view.findViewById(R.id.forward_button);
+        shareToWallButton = (Button) view.findViewById(R.id.share_to_wall_button);
+
+        refreshButton.setOnClickListener(refreshClickListener);
+        backButton.setOnClickListener(goBackClickListener);
+        forwardButton.setOnClickListener(goForwardClickListener);
+        forwardButton.setOnClickListener(shareToWallOnClickListener);
 
         return view;
     }
 
+    protected void setupFragment(){
+        url = getResources().getString(R.string.store_url);
+        withNavigation = true;
+    }
+
+    View.OnClickListener refreshClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            webView.loadUrl(url);
+        }
+    };
+    View.OnClickListener goBackClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            webView.goBack();
+        }
+    };
+    View.OnClickListener goForwardClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            webView.goForward();
+        }
+    };
+    View.OnClickListener shareToWallOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            // TODO
+        }
+    };
 }
