d274627f614b40a41f9a1b1b848db9adf69a3477
Filip Jovanovic <civonavoj.pilif@gmail.com>
2017-01-24 21:43:02 +0100

    TvModel

---
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/model/radio/TvChannel.java
@@ -0,0 +1,72 @@
+package tv.sportssidekick.sportssidekick.model.radio;
+
+import java.util.Date;
+
+/**
+ * Created by Filip on 1/24/2017.
+ * Copyright by Hypercube d.o.o.
+ * www.hypercubesoft.com
+ */
+
+public class TvChannel {
+
+    String id;
+    String name;
+    String url;
+    Date date;
+    String imageUrl;
+
+    public TvChannel(String id, String name, String url, Date date, String imageUrl) {
+
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.date = date;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TvChannel setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TvChannel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public TvChannel setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public TvChannel setDate(Date date) {
+        this.date = date;
+        return this;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public TvChannel setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+        return this;
+    }
+}
