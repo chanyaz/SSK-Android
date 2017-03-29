cc9f4b5b21e1d06b36d470f45bc9102210657b76
Filip Jovanovic <civonavoj.pilif@gmail.com>
2017-03-29 19:31:08 +0200

    Ims - video message thumbnail corrected.

--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/model/Model.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/model/Model.java
@@ -553,9 +553,9 @@ public class Model {
     }
 
     //video_thumb_sLqHBMbL3BQNgddTK0a4wmPfuA531480240556.36911.jpg
-    public void uploadVideoRecordingThumbnail(String filepath){
+    public void uploadVideoRecordingThumbnail(String filepath, File filesDir){
         String filename = "video_thumb_" + currentUserInfo.getUserId() +  System.currentTimeMillis() + ".jpg";
-        AWSFileUploader.getInstance().uploadThumbnail(filename,filepath, GameSparksEvent.Type.VIDEO_IMAGE_FILE_UPLOADED);
+        AWSFileUploader.getInstance().uploadThumbnail(filename,filepath, filesDir, GameSparksEvent.Type.VIDEO_IMAGE_FILE_UPLOADED);
     }
 
     public void uploadImageForProfile(String filepath, File filesDir){
