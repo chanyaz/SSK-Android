b2f53770112825be5afdf3f44507aff9b67c5c13
ndunpeal <n.dunpeal@gmail.com>
2017-03-29 14:50:07 +0200

    Forgot password UI, Handle unsuccessful login.

--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/popup/LoginFragment.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/popup/LoginFragment.java
@@ -1,13 +1,10 @@
 package tv.sportssidekick.sportssidekick.fragment.popup;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -26,11 +23,9 @@ import butterknife.OnClick;
 import tv.sportssidekick.sportssidekick.R;
 import tv.sportssidekick.sportssidekick.fragment.BaseFragment;
 import tv.sportssidekick.sportssidekick.fragment.FragmentEvent;
-import tv.sportssidekick.sportssidekick.fragment.instance.ForgotPasswordFramegnt;
+import tv.sportssidekick.sportssidekick.model.AlertDialogContentManager;
 import tv.sportssidekick.sportssidekick.model.Model;
-import tv.sportssidekick.sportssidekick.service.AlertDialogEvent;
 import tv.sportssidekick.sportssidekick.service.GameSparksEvent;
-import tv.sportssidekick.sportssidekick.util.Utility;
 
 /**
  * Created by Filip on 1/16/2017.
@@ -48,9 +43,12 @@ public class LoginFragment extends BaseFragment {
     AVLoadingIndicatorView progressBar;
     @BindView(R.id.login_text)
     TextView loginText;
+    @BindView(R.id.reset_text)
+    TextView resetText;
 
     @BindView(R.id.forgot_password_container)
     RelativeLayout forgotPasswordContainer;
+
     @BindView(R.id.content_container)
     RelativeLayout loginContainer;
 
@@ -60,8 +58,16 @@ public class LoginFragment extends BaseFragment {
     @BindView(R.id.container)
     RelativeLayout mainContentContainer;
 
-    public LoginFragment() {
+    @BindView(R.id.bottom_buttons_container_reset)
+    RelativeLayout resetButtonContainer;
 
+    @BindView(R.id.bottom_buttons_container_login)
+    RelativeLayout loginButtonContainer;
+
+    @BindView(R.id.login_forgot_pass_email)
+    EditText emailForgotPassword;
+
+    public LoginFragment() {
         // Required empty public constructor
     }
 
@@ -74,43 +80,59 @@ public class LoginFragment extends BaseFragment {
         forgotPasswordBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                slideToRight(loginContainer);
+//                slideToRight(loginContainer);
+//                slideToRight(resetButtonContainer);
+                loginContainer.setVisibility(View.VISIBLE);
+                loginButtonContainer.setVisibility(View.VISIBLE);
+                resetButtonContainer.setVisibility(View.INVISIBLE);
                 forgotPasswordContainer.setVisibility(View.INVISIBLE);
             }
         });
-
         return view;
     }
 
     @OnClick(R.id.bottom_buttons_container_login)
-    public void loginOnClick(){
+    public void loginOnClick() {
         String email = emailEditText.getText().toString();
         String password = passwordEditText.getText().toString();
-        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
-            Toast.makeText(getContext(),"Please enter valid password and displayName!", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(getContext(), "Please enter valid password and displayName!", Toast.LENGTH_SHORT).show();
             return;
         }
-        Model.getInstance().login(email,password);
+        Model.getInstance().login(email, password);
         loginText.setVisibility(View.INVISIBLE);
         progressBar.setVisibility(View.VISIBLE);
+    }
 
+    @OnClick(R.id.reset_text)
+    public void forgotPasswordOnClick() {
+        String email = emailForgotPassword.getText().toString();
+        //TODO FORGOT PASSWROD ON CLICK
+        getActivity().onBackPressed();
     }
 
     @OnClick(R.id.sign_up_button)
-    public void signUpOnClick(){
+    public void signUpOnClick() {
         EventBus.getDefault().post(new FragmentEvent(tv.sportssidekick.sportssidekick.fragment.popup.SignUpFragment.class));
+
     }
 
 
     @OnClick(R.id.forgot_button)
-    public void forgotOnClick(){
+    public void forgotOnClick() {
         // EventBus.getDefault().post(new FragmentEvent(ForgotPasswordFramegnt.class));
-        slideToLeft(loginContainer);
+//        slideToLeft(loginContainer);
+//        slideToLeft(resetButtonContainer);
+
+        loginContainer.setVisibility(View.INVISIBLE);
+        loginButtonContainer.setVisibility(View.INVISIBLE);
+        resetButtonContainer.setVisibility(View.VISIBLE);
+        forgotPasswordContainer.setVisibility(View.VISIBLE);
     }
 
     @Subscribe
-    public void onEvent(GameSparksEvent event){
-        switch (event.getEventType()){
+    public void onEvent(GameSparksEvent event) {
+        switch (event.getEventType()) {
             case SIGNED_OUT:
 //                progressBar.setVisibility(View.GONE);
 //                showLoginForms();
@@ -124,45 +146,33 @@ public class LoginFragment extends BaseFragment {
             case LOGIN_FAILED:
                 progressBar.setVisibility(View.GONE);
                 loginText.setVisibility(View.VISIBLE);
-               // Toast.makeText(getContext(), "Login error", Toast.LENGTH_LONG).show(); // TODO inform user about login failed
-                EventBus.getDefault().post(new FragmentEvent(AlertDialogFragment.class));
-                EventBus.getDefault().post(new AlertDialogEvent("Login error","Try again later."));
+                AlertDialogContentManager.getInstance().showDialog("Login Failed", "Please re-enter your password and try again", null, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                getActivity().onBackPressed();
+                                EventBus.getDefault().post(new FragmentEvent(LoginFragment.class));
+                            }
+                        }
+                );
+                break;
         }
     }
+
     // To animate view slide out from left to right
-    public void slideToRight(View view){
-        TranslateAnimation animate = new TranslateAnimation(0,0,0,0);
+    public void slideToRight(View view) {
+        TranslateAnimation animate = new TranslateAnimation(0,view.getWidth(), 0, 0);
         animate.setDuration(500);
         animate.setFillAfter(true);
         view.startAnimation(animate);
         view.setVisibility(View.VISIBLE);
-        animate.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {}
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                forgotPasswordContainer.setVisibility(View.INVISIBLE);
-            }
-            @Override
-            public void onAnimationRepeat(Animation animation) {}
-        });
     }
+
     // To animate view slide out from right to left
-    public void slideToLeft(View view){
-        TranslateAnimation animate = new TranslateAnimation(0,-view.getWidth(),0,0);
+    public void slideToLeft(View view) {
+        TranslateAnimation animate = new TranslateAnimation(0, -view.getWidth(), 0, 0);
         animate.setDuration(500);
         animate.setFillAfter(true);
         view.startAnimation(animate);
         view.setVisibility(View.INVISIBLE);
-        animate.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {}
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                forgotPasswordContainer.setVisibility(View.VISIBLE);
-            }
-            @Override
-            public void onAnimationRepeat(Animation animation) {}
-        });
     }
 }
