6b4c2e91e1a972cca1177f2e305315be96a03eb0
Filip Jovanovic <civonavoj.pilif@gmail.com>
2017-05-09 17:47:54 +0200

    Merge

--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/activity/LoungeActivity.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/activity/LoungeActivity.java
@@ -8,7 +8,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewTreeObserver;
