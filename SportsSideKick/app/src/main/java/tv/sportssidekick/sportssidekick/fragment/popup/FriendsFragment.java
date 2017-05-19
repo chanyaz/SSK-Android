7565559774ec81ed34fd2d38f1a518cafacaad95
Aleksandar Marinkovic <aleksandar.marinkovic@hypercubesoft.com>
2017-05-19 18:38:16 +0200

    Popup friends  optimaze for telpehone

--- SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/popup/FriendsFragment.java
+++ SportsSideKick/app/src/main/java/tv/sportssidekick/sportssidekick/fragment/popup/FriendsFragment.java
@@ -3,6 +3,7 @@ package tv.sportssidekick.sportssidekick.fragment.popup;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 
+import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Optional;
 import tv.sportssidekick.sportssidekick.R;
 import tv.sportssidekick.sportssidekick.adapter.FriendsAdapter;
 import tv.sportssidekick.sportssidekick.fragment.BaseFragment;
@@ -48,8 +50,8 @@ public class FriendsFragment extends BaseFragment {
 
     @BindView(R.id.progress_bar)
     AVLoadingIndicatorView progressBar;
-
-    @BindView(R.id. friend_requests_count)
+    @Nullable
+    @BindView(R.id.friend_requests_count)
     TextView friendRequestCount;
 
     @BindView(R.id.no_result_text)
@@ -73,12 +75,16 @@ public class FriendsFragment extends BaseFragment {
       /*  int screenWidth = Utility.getDisplayWidth(getActivity());
         int cellSize = (int) (screenWidth * 0.092);
         int columns = (screenWidth / (cellSize + (getResources().getDimensionPixelSize(R.dimen.margin_15) * 2)));*/
-       // GridLayoutManager layoutManager = new GridLayoutManager(getContext(), columns);
-       // friendsRecyclerView.setLayoutManager(layoutManager);
-       // friendsRecyclerView.addItemDecoration(new GridItemDecoration(getResources().getDimensionPixelSize(R.dimen.margin_20),columns));
+        // GridLayoutManager layoutManager = new GridLayoutManager(getContext(), columns);
+        // friendsRecyclerView.setLayoutManager(layoutManager);
+        // friendsRecyclerView.addItemDecoration(new GridItemDecoration(getResources().getDimensionPixelSize(R.dimen.margin_20),columns));
 
         int screenWidth = Utility.getDisplayWidth(getActivity());
-        friendsRecyclerView.setCellWidth((int) (screenWidth * GRID_PERCENT_CELL_WIDTH));
+        if (getResources().getBoolean(R.bool.is_tablet))
+            friendsRecyclerView.setCellWidth((int) (screenWidth * GRID_PERCENT_CELL_WIDTH));
+        else
+            friendsRecyclerView.setCellWidth((int) (screenWidth * (GRID_PERCENT_CELL_WIDTH * 2.4)));
+
 
         friendsRecyclerView.addItemDecoration(new AutofitDecoration(getActivity()));
         friendsRecyclerView.setHasFixedSize(true);
@@ -88,11 +94,11 @@ public class FriendsFragment extends BaseFragment {
 
         adapter.setInitiatorFragment(FriendsFragment.class);
         friendsRecyclerView.setAdapter(adapter);
-        Task<List<UserInfo>> friendsTask =  FriendsManager.getInstance().getFriends(0);
+        Task<List<UserInfo>> friendsTask = FriendsManager.getInstance().getFriends(0);
         friendsTask.addOnCompleteListener(new OnCompleteListener<List<UserInfo>>() {
             @Override
             public void onComplete(@NonNull Task<List<UserInfo>> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     noResultText.setVisibility(View.GONE);
                     friendsRecyclerView.setVisibility(View.VISIBLE);
                     friends = task.getResult();
@@ -103,6 +109,7 @@ public class FriendsFragment extends BaseFragment {
                     noResultText.setVisibility(View.VISIBLE);
                     friendsRecyclerView.setVisibility(View.GONE);
                 }
+
                 progressBar.setVisibility(View.GONE);
             }
         });
@@ -111,15 +118,19 @@ public class FriendsFragment extends BaseFragment {
         task.addOnCompleteListener(new OnCompleteListener<List<FriendRequest>>() {
             @Override
             public void onComplete(@NonNull Task<List<FriendRequest>> task) {
-                if(task.isSuccessful()){
-                    if(task.getResult()!=null){
-                        friendRequestCount.setText(String.valueOf(task.getResult().size()));
+                if (task.isSuccessful()) {
+                    if (task.getResult() != null) {
+
+                        if (friendRequestCount != null)
+                            friendRequestCount.setText(String.valueOf(task.getResult().size()));
                         return;
                     }
                 }
-                friendRequestCount.setText("0");
+                if (friendRequestCount != null)
+                    friendRequestCount.setText("0");
             }
         });
+
 
         searchText.addTextChangedListener(new TextWatcher() {
 
@@ -133,8 +144,7 @@ public class FriendsFragment extends BaseFragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (friends != null)
-                {
+                if (friends != null) {
                     adapter.getValues().clear();
                     adapter.getValues().addAll(Utility.filter(friends, s.toString()));
                     adapter.notifyDataSetChanged();
@@ -145,24 +155,27 @@ public class FriendsFragment extends BaseFragment {
         return view;
     }
 
+    @Optional
     @OnClick(R.id.confirm_button)
-    public void confirmOnClick(){
+    public void confirmOnClick() {
         getActivity().onBackPressed();
     }
 
+    @Optional
     @OnClick(R.id.close_dialog_button)
-    public void closeDialog(){
+    public void closeDialog() {
         getActivity().onBackPressed();
     }
 
+    @Optional
     @OnClick(R.id.friend_requests)
-    public void friendRequestsDialog(){
+    public void friendRequestsDialog() {
         EventBus.getDefault().post(new FragmentEvent(FriendRequestsFragment.class));
     }
 
+    @Optional
     @OnClick(R.id.add_friend)
-    public void  addFriend()
-    {
+    public void addFriend() {
         EventBus.getDefault().post(new FragmentEvent(AddFriendFragment.class));
     }
 }
