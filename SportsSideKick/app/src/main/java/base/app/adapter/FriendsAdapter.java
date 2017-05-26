8d49a4519226fcbf2fc643ef6469997f0877f505
Aleksandar Marinkovic <aleksandar.marinkovic@hypercubesoft.com>
2017-05-26 18:28:52 +0200

    Popup adds friends optimize for telephone

--- SportsSideKick/app/src/main/java/base/app/adapter/FriendsAdapter.java
+++ SportsSideKick/app/src/main/java/base/app/adapter/FriendsAdapter.java
@@ -1,14 +1,18 @@
 package base.app.adapter;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
@@ -17,6 +21,7 @@ import org.greenrobot.eventbus.EventBus;
 import java.util.ArrayList;
 import java.util.List;
 
+import base.app.model.friendship.FriendsManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import base.app.R;
@@ -35,6 +40,10 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
 
     private static final String TAG = "Friends Adapter";
 
+    public void setValues(List<UserInfo> values) {
+        this.values = values;
+    }
+
     private List<UserInfo> values;
 
     public void setInitiatorFragment(Class initiatorFragment) {
@@ -49,6 +58,8 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
 
     private int screenWidth;
 
+    int layout = -1;
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         public View view;
         @Nullable
@@ -60,6 +71,12 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
         @Nullable
         @BindView(R.id.profile_name)
         TextView name;
+        @Nullable
+        @BindView(R.id.number_mutual_friends)
+        TextView numberMutualFriends;
+        @Nullable
+        @BindView(R.id.button_add_friend)
+        ImageButton buttonAddFriend;
 
         ViewHolder(View v) {
             super(v);
@@ -72,6 +89,7 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
         this.initiatorFragment = initiatorFragment;
         values = new ArrayList<>();
         this.screenWidth = 0;
+        layout = -1;
     }
 
     @Override
@@ -79,26 +97,65 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
         return 0;
     }
 
+    public void setLayout(int layout) {
+        this.layout = layout;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
         final ViewHolder viewHolder;
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_item, parent, false);
+        View view;
+        if (layout == -1)
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.friend_item, parent, false);
+        else {
+            view = LayoutInflater.from(parent.getContext()).inflate(layout, parent, false);
+        }
         viewHolder = new ViewHolder(view);
         if (screenWidth != 0) {
             view.getLayoutParams().height = RelativeLayout.LayoutParams.MATCH_PARENT;
 
             view.getLayoutParams().width = screenWidth;
         }
-        viewHolder.view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentEvent fragmentEvent = new FragmentEvent(MemberInfoFragment.class);
-                fragmentEvent.setInitiatorFragment(initiatorFragment);
-                int position = viewHolder.getLayoutPosition();
-                fragmentEvent.setId(values.get(position).getUserId());
-                EventBus.getDefault().post(fragmentEvent);
-            }
-        });
+        if (layout == -1) {
+            viewHolder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    FragmentEvent fragmentEvent = new FragmentEvent(MemberInfoFragment.class);
+                    fragmentEvent.setInitiatorFragment(initiatorFragment);
+                    int position = viewHolder.getLayoutPosition();
+                    fragmentEvent.setId(values.get(position).getUserId());
+                    EventBus.getDefault().post(fragmentEvent);
+                }
+            });
+        } else if (layout == R.layout.row_add_friend_item && viewHolder.buttonAddFriend != null) {
+            viewHolder.buttonAddFriend.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                        Task<UserInfo> manageFriendTask;
+                        final UserInfo user = values.get(viewHolder.getAdapterPosition());
+                        if (values.get(viewHolder.getAdapterPosition()).isaFriend()) { // this user is my friend, remove it from friends
+                            manageFriendTask = FriendsManager.getInstance().deleteFriend(user.getUserId());
+                        } else { // send friend request
+                            manageFriendTask = FriendsManager.getInstance().sendFriendRequest(user.getUserId());
+                        }
+                        manageFriendTask.addOnCompleteListener(new OnCompleteListener<UserInfo>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UserInfo> task) {
+                                if (task.isSuccessful()) {
+                                    user.setaFriend(!user.isaFriend());
+                                    if (user.isaFriend()) {
+                                        viewHolder.buttonAddFriend.setSelected(true);
+                                    } else {
+                                        viewHolder.buttonAddFriend.setSelected(false);
+                                    }
+                                }
+                            }
+                        });
+                    }
+
+            });
+        }
         return viewHolder;
     }
 
@@ -107,16 +164,28 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.ViewHold
         final UserInfo info = values.get(position);
         DisplayImageOptions imageOptions = Utility.getImageOptionsForUsers();
         String avatarUrl = info.getCircularAvatarUrl();
+
         if (avatarUrl != null) {
             ImageLoader.getInstance().displayImage(avatarUrl, holder.avatar, imageOptions);
         }
-
         if (info.getIsOnline()) {
-            holder.online.setVisibility(View.VISIBLE);
+            if (holder.online != null) {
+                holder.online.setVisibility(View.VISIBLE);
+            }
         } else {
-            holder.online.setVisibility(View.GONE);
+            if (holder.online != null) {
+                holder.online.setVisibility(View.GONE);
+            }
         }
-        holder.name.setText(info.getLastName() + " " + info.getFirstName());
+        if (holder.name != null) {
+            holder.name.setText(info.getLastName() + " " + info.getFirstName());
+        }
+        if(holder.buttonAddFriend != null)
+        if (info.isaFriend() )
+            holder.buttonAddFriend.setSelected(true);
+        else
+            holder.buttonAddFriend.setSelected(false);
+
     }
 
     @Override
--- SportsSideKick/app/src/main/java/base/app/fragment/popup/AddFriendFragment.java
+++ SportsSideKick/app/src/main/java/base/app/fragment/popup/AddFriendFragment.java
@@ -3,10 +3,10 @@ package base.app.fragment.popup;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
-import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,8 +21,11 @@ import com.wang.avi.AVLoadingIndicatorView;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import base.app.adapter.FindOfficialAdapter;
+import base.app.model.friendship.FriendsManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -33,6 +36,7 @@ import base.app.fragment.FragmentEvent;
 import base.app.model.friendship.PeopleSearchManager;
 import base.app.model.user.UserInfo;
 import base.app.util.Utility;
+import butterknife.Optional;
 
 /**
  * Created by Djordje Krutil on 29.3.2017..
@@ -43,28 +47,132 @@ public class AddFriendFragment extends BaseFragment {
 
     @BindView(R.id.people_recycler_view)
     RecyclerView people;
-
+    public static final double GRID_PERCENT_CELL_WIDTH_PHONE = 0.2;
     @BindView(R.id.add_friend_name)
     EditText friendName;
-
+    @Nullable
+    @BindView(R.id.special_recycler_view)
+    RecyclerView specialRecyclerView;
+    @Nullable
+    @BindView(R.id.special_list_holder)
+    RelativeLayout specialListContainer;
     @BindView(R.id.progress_bar)
     AVLoadingIndicatorView progressBar;
-
+    @Nullable
+    @BindView(R.id.no_result_special)
+    TextView noResultSpecialText;
     @BindView(R.id.no_result)
     TextView noResultCaption;
-
+    @Nullable
+    @BindView(R.id.common_friend_list)
+    RecyclerView commonFriend;
     @BindView(R.id.friends_list)
     RelativeLayout listContainer;
-
+    FindOfficialAdapter officialAdapter;
+    boolean isTablet;
+    List<UserInfo> specialUserInfoList;
+    FriendsAdapter adapter;
+    FriendsAdapter commonFriendAdapter;
+    List<Integer> mutualFriendsNumberList;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.popup_add_friend, container, false);
         ButterKnife.bind(this, view);
+        isTablet = getResources().getBoolean(R.bool.is_tablet);
+        adapter = new FriendsAdapter(this.getClass());
+        adapter.setInitiatorFragment(this.getClass());
+        if (isTablet) {
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+            people.setLayoutManager(layoutManager);
+            people.setAdapter(adapter);
+        } else {
+            setCommonFriendAdapter();
+            adapter.setLayout(R.layout.row_add_friend_item);
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+            people.setLayoutManager(layoutManager);
+            people.setAdapter(adapter);
+            setSpecialUserList();
+        }
+        setTextWatcher();
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
-        people.setLayoutManager(layoutManager);
+        return view;
+    }
 
+    @Optional
+    @OnClick(R.id.invite_friend_button)
+    public void onClickInviteFriend() {
+        EventBus.getDefault().post(new FragmentEvent(InviteFriendFragment.class));
+    }
+
+    private void onDataSetChange(boolean visible) {
+        if (visible) {
+            noResultCaption.setVisibility(View.GONE);
+            people.setVisibility(View.VISIBLE);
+            listContainer.setVisibility(View.VISIBLE);
+        } else {
+            noResultCaption.setVisibility(View.VISIBLE);
+            listContainer.setVisibility(View.VISIBLE);
+            people.setVisibility(View.GONE);
+        }
+
+
+    }
+
+
+    private void onSpecialDataSetChange(boolean visible) {
+        if (specialListContainer != null && specialRecyclerView != null && noResultSpecialText != null)
+            if (visible) {
+                noResultSpecialText.setVisibility(View.GONE);
+                specialRecyclerView.setVisibility(View.VISIBLE);
+                specialListContainer.setVisibility(View.VISIBLE);
+            } else {
+                specialListContainer.setVisibility(View.VISIBLE);
+                specialRecyclerView.setVisibility(View.INVISIBLE);
+                noResultSpecialText.setVisibility(View.VISIBLE);
+            }
+
+
+    }
+
+
+    private void setSpecialUserList() {
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 2);
+        //   LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        specialUserInfoList = new ArrayList<>();
+        assert specialRecyclerView != null;
+        specialRecyclerView.setLayoutManager(gridLayoutManager);
+        officialAdapter = new FindOfficialAdapter(getActivity(), this.getClass());
+        specialRecyclerView.setAdapter(officialAdapter);
+        onDataSetChange(false);
+        listContainer.setVisibility(View.INVISIBLE);
+        //TODO @Filip, add new api call, get all admins.
+        Task<List<UserInfo>> peopleTask = PeopleSearchManager.getInstance().searchPeople("", 0);
+        peopleTask.addOnCompleteListener(new OnCompleteListener<List<UserInfo>>() {
+            @Override
+            public void onComplete(@NonNull Task<List<UserInfo>> task) {
+                if (task.isSuccessful()) {
+                    if (task.getResult().size() > 0) {
+                        for (UserInfo userInfo : task.getResult())
+                            if (userInfo.getUserType().equals(UserInfo.UserType.special))
+                                specialUserInfoList.add(userInfo);
+                        if (!specialUserInfoList.isEmpty())
+                            officialAdapter.setValues(specialUserInfoList);
+                        officialAdapter.notifyDataSetChanged();
+                        onSpecialDataSetChange(true);
+                    } else {
+                        onSpecialDataSetChange(false);
+                    }
+                } else {
+                    onSpecialDataSetChange(false);
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setTextWatcher() {
         friendName.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -73,58 +181,93 @@ public class AddFriendFragment extends BaseFragment {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (count == 0) {
+                    listContainer.setVisibility(View.GONE);
+                    if (specialListContainer != null) {
+                        specialListContainer.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    listContainer.setVisibility(View.VISIBLE);
+                    if (specialListContainer != null) {
+                        specialListContainer.setVisibility(View.GONE);
+                    }
+                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (TextUtils.isEmpty(s))
-                {
-                    listContainer.setVisibility(View.GONE);
-                }
-                else {
-                    final FriendsAdapter adapter = new FriendsAdapter(this.getClass());
-                    adapter.setInitiatorFragment(this.getClass());
-                    people.setAdapter(adapter);
-                    String text = s.toString();
-                    Task<List<UserInfo>> peopleTask = PeopleSearchManager.getInstance().searchPeople(text,0);
-                    peopleTask.addOnCompleteListener(new OnCompleteListener<List<UserInfo>>() {
-                        @Override
-                        public void onComplete(@NonNull Task<List<UserInfo>> task) {
-                            if(task.isSuccessful()){
-                                if (task.getResult().size() >0)
-                                {
-                                    noResultCaption.setVisibility(View.GONE);
-                                    people.setVisibility(View.VISIBLE);
-                                    listContainer.setVisibility(View.VISIBLE);
-                                    adapter.getValues().addAll(task.getResult());
-                                    adapter.notifyDataSetChanged();
-                                }
-                                else {
-                                    noResultCaption.setVisibility(View.VISIBLE);
-                                    listContainer.setVisibility(View.VISIBLE);
-                                    people.setVisibility(View.GONE);
-                                }
+
+
+                String text = s.toString();
+                //TODO @Filip, add item in the Api mutual friends List.
+                Task<List<UserInfo>> peopleTask = PeopleSearchManager.getInstance().searchPeople(text, 0);
+                peopleTask.addOnCompleteListener(new OnCompleteListener<List<UserInfo>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<UserInfo>> task) {
+                        if (task.isSuccessful()) {
+                            if (task.getResult().size() > 0) {
+                                onDataSetChange(true);
+                                adapter.setValues(task.getResult());
+                                adapter.notifyDataSetChanged();
+                                if (!isTablet)
+                                    commonFriendAddItems(task.getResult());
                             } else {
-                                noResultCaption.setVisibility(View.VISIBLE);
-                                listContainer.setVisibility(View.VISIBLE);
-                                people.setVisibility(View.GONE);
+                                onDataSetChange(false);
                             }
-                            progressBar.setVisibility(View.GONE);
-//                            if (view != null)
-//                            {
-//                                Utility.hideKeyboardFrom(getContext(), view);
-//                            }
+                        } else {
+                            onDataSetChange(false);
                         }
-                    });
-                }
+                        progressBar.setVisibility(View.GONE);
+//
+                    }
+                });
+
             }
         });
-
-        return view;
     }
 
-    @OnClick(R.id.invite_friend_button)
-    public void onClickInviteFriend() {
-        EventBus.getDefault().post(new FragmentEvent(InviteFriendFragment.class));
+    private void commonFriendAddItems(final List<UserInfo> result) {
+
+        commonFriendAdapter.setValues(new ArrayList<UserInfo>());
+        for (final UserInfo userInfo : result) {
+            Task<List<UserInfo>> peopleTask = FriendsManager.getInstance().getMutualFriendsListWithUser(userInfo.getUserId(), 0);
+            peopleTask.addOnCompleteListener(new OnCompleteListener<List<UserInfo>>() {
+                @Override
+                public void onComplete(@NonNull Task<List<UserInfo>> task) {
+                    if (task.isSuccessful()) {
+                        if (task.getResult().size() > 0) {
+                            commonFriendAdapter.getValues().addAll(task.getResult());
+                            commonFriendAdapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            });
+        }
     }
+
+    @Optional
+    @OnClick(R.id.your_friends_open_button)
+    public void friendsOnClick() {
+        EventBus.getDefault().post(new FragmentEvent(FriendsFragment.class));
+    }
+
+
+    @Optional
+    @OnClick(R.id.friend_requests)
+    public void profileOnClick() {
+        EventBus.getDefault().post(new FragmentEvent(FriendRequestsFragment.class));
+    }
+
+    public void setCommonFriendAdapter() {
+        if (commonFriend != null) {
+            int screenWidth = Utility.getDisplayWidth(getActivity());
+            commonFriendAdapter = new FriendsAdapter(this.getClass());
+            LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+            commonFriend.setLayoutManager(layoutManager);
+            commonFriendAdapter.screenWidth((int) (screenWidth * GRID_PERCENT_CELL_WIDTH_PHONE));
+            // commonFriend.setHasFixedSize(true);
+            commonFriend.setAdapter(commonFriendAdapter);
+        }
+    }
+
 }
