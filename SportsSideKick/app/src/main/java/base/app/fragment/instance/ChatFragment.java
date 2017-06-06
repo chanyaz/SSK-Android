536469f61be42f638a5b0c8e7998c271baa23bdc
Aleksandar Marinkovic <aleksandar.marinkovic@hypercubesoft.com>
2017-06-06 12:17:15 +0200

    Chat fragment-when user is logged u cant see chats - fix

--- SportsSideKick/app/src/main/java/base/app/fragment/instance/ChatFragment.java
+++ SportsSideKick/app/src/main/java/base/app/fragment/instance/ChatFragment.java
@@ -187,8 +187,8 @@ public class ChatFragment extends BaseFragment {
         snappyLinearLayoutManager.setSnapDuration(1000);
         snappyLinearLayoutManager.setSeekDuration(1000);
         messageListView.setLayoutManager(snappyLinearLayoutManager);
-
-
+        if (!Utility.isTablet(getActivity()))
+            ImsManager.getInstance().reload();
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
