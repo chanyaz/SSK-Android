e5d070cee787b4a73b6998b143e32122df602f89
Filip Jovanovic <civonavoj.pilif@gmail.com>
2017-10-05 13:55:42 +0200

    Fixed SPO1024 and SPO1026

--- SportsSideKick/app/src/main/java/base/app/fragment/instance/NewsItemFragment.java
+++ SportsSideKick/app/src/main/java/base/app/fragment/instance/NewsItemFragment.java
@@ -453,10 +453,10 @@ public class NewsItemFragment extends BaseFragment {
     public void readMoreClick(View view) {
         if (content.getMaxLines() == 3) {
             content.setMaxLines(Integer.MAX_VALUE);
-            ((TextView)view).setText(R.string.read_more_closed);
+            ((TextView)view).setText(R.string.read_more_open);
         } else {
             content.setMaxLines(3);
-            ((TextView)view).setText(R.string.read_more_open);
+            ((TextView)view).setText(R.string.read_more_closed);
         }
     }
 
