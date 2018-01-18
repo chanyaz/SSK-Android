556bdaf4a16f0a477d9984bfb5bb8761f6b44893
Alex Sheiko <alexsheikodev@gmail.com>
2018-01-18 20:06:24 +0200

    fix news and rumours loading

--- SportsSideKick/app/src/main/java/base/app/data/wall/BaseItem.kt
+++ SportsSideKick/app/src/main/java/base/app/data/wall/BaseItem.kt
@@ -9,8 +9,8 @@ import java.io.Serializable
 @JsonIgnoreProperties(ignoreUnknown = true)
 abstract class BaseItem : Serializable {
 
-    lateinit var postId: String
-    lateinit var wallId: String
+    var postId: String = ""
+    var wallId: String = ""
     var likeCount = 0
     var shareCount = 0
     var commentsCount = 0
