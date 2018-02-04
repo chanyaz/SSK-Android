f25d8dbca8967a1c31a35c1c35a8bb607f4c776a
Alex Sheiko <alexsheikodev@gmail.com>
2018-02-04 08:58:38 +0200

    implement view inflater changes

--- SportsSideKick/app/src/main/java/base/app/util/ui/ViewInflater.kt
+++ SportsSideKick/app/src/main/java/base/app/util/ui/ViewInflater.kt
@@ -4,16 +4,23 @@ import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 
-fun View.setVisible(visible: Boolean) {
-    visibility = if (visible) {
-        VISIBLE
-    } else {
-        GONE
-    }
+fun ViewGroup.inflate(layoutId: Int): View {
+    return LayoutInflater.from(context).inflate(
+            layoutId, this, false)
+}
+
+fun View.visible() {
+    visibility = VISIBLE
+}
+
+fun View.hide() {
+    visibility = GONE
 }
 
 inline fun <reified T : ViewModel> Fragment.inject(): T {
