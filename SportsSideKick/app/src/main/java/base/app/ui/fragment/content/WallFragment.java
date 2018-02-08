cc3375f050c588018d266e053b3010724e4b58c3
Alex Sheiko <alexsheikodev@gmail.com>
2018-02-08 18:57:13 +0200

    show next match images on android 4

--- SportsSideKick/app/src/main/java/base/app/ui/fragment/content/WallFragment.java
+++ SportsSideKick/app/src/main/java/base/app/ui/fragment/content/WallFragment.java
@@ -170,8 +170,8 @@ public class WallFragment extends BaseFragment implements LoginStateReceiver.Log
             if(NextMatchModel.getInstance().isNextMatchUpcoming()){
                 nextMatchContainer.setVisibility(View.VISIBLE);
                 NewsTickerInfo newsTickerInfo = NextMatchModel.getInstance().getTickerInfo();
-                ImageLoader.displayImage(newsTickerInfo.getFirstClubUrl(), wallLeftTeamImage, null);
-                ImageLoader.displayImage(newsTickerInfo.getSecondClubUrl(), wallRightTeamImage, null);
+                ImageLoader.displayImage("http://desporto_stats.imgs.sapo.pt/3/Teams/M74559.png", wallLeftTeamImage);
+                ImageLoader.displayImage(newsTickerInfo.getSecondClubUrl(), wallRightTeamImage);
                 wallLeftTeamName.setText(newsTickerInfo.getFirstClubName());
                 wallRightTeamName.setText(newsTickerInfo.getSecondClubName());
                 long timestamp = Long.parseLong(newsTickerInfo.getMatchDate());
