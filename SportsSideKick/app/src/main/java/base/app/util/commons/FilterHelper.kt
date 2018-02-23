9bdd2024238404810d082a6c1ed72f35ad7e1e3d
Alex Sheiko <alexsheikodev@gmail.com>
2018-02-23 13:48:48 +0200

    dont overlap menu dots

--- SportsSideKick/app/src/main/java/base/app/util/commons/FilterHelper.kt
+++ SportsSideKick/app/src/main/java/base/app/util/commons/FilterHelper.kt
@@ -7,8 +7,8 @@ fun filterMessages(list: ArrayList<ImsMessage>): List<ImsMessage> {
     return list.filterNot { it.type == GSConstants.UPLOAD_TYPE_IMAGE && it.imageUrl == null }
             .filterNot { it.type == GSConstants.UPLOAD_TYPE_VIDEO && it.vidUrl == null }
             .filterNot { it.type == GSConstants.UPLOAD_TYPE_TEXT && (it.text == null || it.text.isBlank()) }
-            // old broken messages stuck in loading mode
             .filterNot {
+                // old broken messages stuck in loading mode
                 it.id == "59e48ffac5b51e04d4e2534a"
                         || it.id == "59f6fa57c5b51e04d4b790fe"
                         || it.id == "59f6fbebcc302a04d34bd5bd"
