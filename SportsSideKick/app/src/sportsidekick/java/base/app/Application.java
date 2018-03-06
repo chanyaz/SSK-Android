fb9fab7709b51d180dee540117c8f1093e9c2191
Alex Sheiko <alexsheikodev@gmail.com>
2018-03-06 23:43:29 +0200

    init amazon analytics client

--- SportsSideKick/app/src/sportsidekick/java/base/app/Application.java
+++ SportsSideKick/app/src/sportsidekick/java/base/app/Application.java
@@ -6,7 +6,10 @@ import android.support.multidex.MultiDexApplication;
 import android.support.text.emoji.EmojiCompat;
 import android.support.text.emoji.bundled.BundledEmojiCompatConfig;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration;
 import com.amazonaws.mobileconnectors.pinpoint.PinpointManager;
+import com.amazonaws.regions.Regions;
 import com.facebook.appevents.AppEventsLogger;
 import com.miguelbcr.ui.rx_paparazzo2.RxPaparazzo;
 import com.pixplicity.easyprefs.library.Prefs;
@@ -24,13 +27,12 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  */
 public class Application extends MultiDexApplication {
 
-    private static AppEventsLogger sTracker;
-    private PinpointManager pinpointManager;
+    private static PinpointManager sTracker;
 
     /**
      * Gets the default {@link AppEventsLogger} for this {@link Application}.
      */
-    public static AppEventsLogger getDefaultTracker() {
+    public static PinpointManager getDefaultTracker() {
         return sTracker;
     }
 
@@ -66,12 +68,10 @@ public class Application extends MultiDexApplication {
 
         RxPaparazzo.register(this);
 
-        sTracker = AppEventsLogger.newLogger(this);
-        /*TODO if (BuildConfig.DEBUG)*/
+        CognitoCachingCredentialsProvider cognitoCachingCredentialsProvider = new CognitoCachingCredentialsProvider(this, "eu-west-2:ca9f6037-6a92-4484-9178-b1575679a327", Regions.US_WEST_2);
 
-        // Start a session with Pinpoint
-        pinpointManager.getSessionClient().startSession();
-
+        PinpointConfiguration pinpointConfig = new PinpointConfiguration(this, "4b503a0c6c0c4480ac2feb157b546a87", Regions.US_EAST_1, cognitoCachingCredentialsProvider);
+        sTracker = new PinpointManager(pinpointConfig);
 
         EmojiCompat.Config config = new BundledEmojiCompatConfig(this);
         EmojiCompat.init(config);
